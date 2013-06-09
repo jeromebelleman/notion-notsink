b3b09d8b1713d8f5e8b1206ba1365a13bccb8186
Jerome Belleman <jerome.belleman@gmail.com>
2013-06-09 19:18:26 +0200

    Add configuration and features

---
+++ notsink.lua
@@ -0,0 +1,23 @@
+--[[
+-- Notification sink, e.g. for urxvt -name notsink windows
+--]]
+
+local NAME = 'notsink'
+local ATTR = 'sink'
+
+defwinprop{
+    instance = NAME,
+    target = NAME,
+}
+
+function notsink_set() -- FIXME Not global please, use pkg and all
+    -- XXX Difference between a parent and a manager?
+    cur = ioncore.current()
+    if obj_is(cur, 'WFrame') then
+        cur:set_name(NAME)
+        cur:set_grattr(ATTR, 'set')
+    elseif obj_is(cur, 'WClientWin') then
+        cur:parent():set_name(NAME)
+        cur:parent():set_grattr(ATTR, 'set')
+    end
+end
